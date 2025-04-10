d21fbd5f099eae1bbf6399723af9f791e90e3b09
Franco Adrian Luna Colchicini <francomoon99@gmail.com>
2025-04-10 11:50:01 -0300

    Add files via upload

---
+++ 04 Estructuras Repetitivas/tp4-2.py
@@ -0,0 +1,12 @@
+"""
+2) Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de
+dígitos que contiene.
+"""
+
+num=int(input("Ingresar un número entero: "))
+contador=0
+while num > 1:
+    num = num % 10
+    contador += 1
+
+print("La cantidad de digitos son",contador)

ee63ab119bf59fac9757f9e0399d61e7a1ef0431
Franco Adrian Luna Colchicini <francomoon99@gmail.com>
2025-04-10 12:02:46 -0300

    Update tp4-3.py

--- 04 Estructuras Repetitivas/tp4-3.py
+++ 04 Estructuras Repetitivas/tp4-3.py
@@ -1,1 +1,20 @@
-print("hola")
+"""
+3) Escribe un programa que sume todos los números enteros comprendidos entre dos valores
+dados por el usuario, excluyendo esos dos valores.
+
+"""
+
+num1=int(input("Ingresar primer número: "))
+num2=int(input("Ingresar segundo número: "))
+acumulador=0
+
+if num2>num1:
+    for i in range (num1+1,num2,1):
+        print (i)
+        acumulador = acumulador + i
+else:
+    for i in range (num2+1,num1,1):
+        print (i)
+        acumulador = acumulador + i
+
+print(f"La suma de todos los números entre el primer y el segundo número ingresado es {acumulador}")

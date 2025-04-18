6fcff8c709e5b22af8267c5aec50aa3a0fc01d07
Franco Adrian Luna Colchicini <francomoon99@gmail.com>
2025-04-18 20:15:25 -0300

    Create tp5-10.py

---
+++ 05 Funciones/tp5-10.py
@@ -0,0 +1,18 @@
+"""
+10.Crear una función llamada calcular_promedio(a, b, c) que reciba
+tres números como parámetros y devuelva el promedio de ellos.
+Solicitar los números al usuario y mostrar el resultado usando esta
+función.
+"""
+# función calcular_promedio(a, b, c)
+def calcular_promedio(a, b, c):
+    promedio = (a+b+c)/3
+    return promedio
+
+num1 = float(input("Ingresar el primer valor: "))
+num2 = float(input("Ingresar el segundo valor: "))
+num3 = float(input("Ingresar el tercer valor: "))
+
+promedio = calcular_promedio(num1,num2,num3)
+
+print (f"El promedio de los 3 valores previos es {promedio}")

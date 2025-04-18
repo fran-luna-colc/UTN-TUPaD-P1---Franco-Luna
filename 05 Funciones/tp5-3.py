f10e6e8eab2d2bd1fd2159f633c4d37395249a08
Franco Adrian Luna Colchicini <francomoon99@gmail.com>
2025-04-18 13:01:41 -0300

    Create tp5-3.py

---
+++ 05 Funciones/tp5-3.py
@@ -0,0 +1,16 @@
+"""
+3. Crear una función llamada informacion_personal(nombre, apellido,
+edad, residencia) que reciba cuatro parámetros e imprima: “Soy
+[nombre] [apellido], tengo [edad] años y vivo en [residencia]”. 
+Pedir los datos al usuario y llamar a esta función con los valores ingresados.
+"""
+
+def informacion_personal(nombre, apellido, edad, residencia):
+    return (f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+nombre =input("Ingresar nombre: ")
+apellido =input("Ingresar apellido: ")
+edad =input("Ingresas edad: ")
+residencia =input("Ingresar residencia: ")
+
+print (informacion_personal(nombre,apellido,edad,residencia))

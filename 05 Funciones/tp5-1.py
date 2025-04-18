06735f3ee633ac8d06aa8b3ad473f7e492a99589
Franco Adrian Luna Colchicini <francomoon99@gmail.com>
2025-04-18 00:57:15 -0300

    Create tp5-1.py

---
+++ 05 Funciones/tp5-1.py
@@ -0,0 +1,10 @@
+"""
+Crear una función llamada imprimir_hola_mundo que imprima por
+pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+programa principal.
+"""
+#Función nueva imprimir_hola_mundo
+def imprimir_hola_mundo():
+    return ("Hola Mundo!")
+#Función principal / Programa principal
+print(imprimir_hola_mundo())

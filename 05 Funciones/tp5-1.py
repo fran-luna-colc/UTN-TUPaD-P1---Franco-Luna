4922b40e2fad12adfe8aa7bbb89581834981d33e
Franco Adrian Luna Colchicini <francomoon99@gmail.com>
2025-04-18 22:14:26 -0300

    Update tp5-1.py

--- 05 Funciones/tp5-1.py
+++ 05 Funciones/tp5-1.py
@@ -1,10 +1,11 @@
 """
-Crear una función llamada imprimir_hola_mundo que imprima por
+1. Crear una función llamada imprimir_hola_mundo que imprima por
 pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
 programa principal.
 """
 #Función nueva imprimir_hola_mundo
-def imprimir_hola_mundo():
-    return ("Hola Mundo!")
+def imprimir_hola_mundo(): #definición de una nueva función
+    return ("Hola Mundo!") #resultado de invocar la función
+
 #Función principal / Programa principal
-print(imprimir_hola_mundo())
+print(imprimir_hola_mundo()) #invocar la función

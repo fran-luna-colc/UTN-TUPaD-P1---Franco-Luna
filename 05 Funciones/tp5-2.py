7c4dd0799e1d6d24bc6510b4c44b37f2c53bba38
Franco Adrian Luna Colchicini <francomoon99@gmail.com>
2025-04-18 22:13:55 -0300

    Update tp5-2.py

--- 05 Funciones/tp5-2.py
+++ 05 Funciones/tp5-2.py
@@ -1,13 +1,14 @@
 """
-Crear una función llamada saludar_usuario(nombre) que reciba
+2. Crear una función llamada saludar_usuario(nombre) que reciba
 como parámetro un nombre y devuelva un saludo personalizado.
 Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. Llamar a esta función desde el programa
 principal solicitando el nombre al usuario.
 """
-
+# Función nueva saludar_usuario
 def saludar_usuario(nombre):
     return (f"Buenos días {nombre}!")
 
+# Función principal / Programa principal
 nombre=input("Ingresar nombre: ")
 
 print(saludar_usuario(nombre))
